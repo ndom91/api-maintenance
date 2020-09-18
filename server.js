506c00c0fecf5819d37f3dbf73713b4a19a625ff
Nico Domino <ndomino@newtelco.de>
2020-09-18 14:15:38 +0000

    update: algoliasearch deprecated methods

--- server.js
+++ server.js
@@ -555,7 +555,9 @@ app.post('/v1/api/search/update', cors(corsOptions), (req, res) => {
       )
       const index = client.initIndex(process.env.ALGOLIA_INDEX)
 
-      index.addObjects([results[0]], (err, content) => {
+      index.saveObjects([results[0]], {
+				autoGenerateObjectIDIfNotExist: true
+				}, (err, content) => {
         if (err) {
           console.error(err)
           res.json({ id: maintId, error: err })
